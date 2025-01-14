9d13ccbac66ded97c0fcefb020edc18de52548df
luke bullock <111459215+TrueGlimGrold@users.noreply.github.com>
2025-01-13 21:08:51 -0700

    modifying auth

--- nextjs-dashboard/auth.config.ts
+++ nextjs-dashboard/auth.config.ts
@@ -1,21 +1,25 @@
 import type { NextAuthConfig } from 'next-auth';
- 
-export const authConfig = {
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export const authConfig: NextAuthConfig = {
   pages: {
     signIn: '/login',
   },
   callbacks: {
-    authorized({ auth, request: { nextUrl } }) {
+    async authorized({ request, auth }: { request: NextRequest; auth: { user?: unknown } | null }) {
       const isLoggedIn = !!auth?.user;
-      const isOnDashboard = nextUrl.pathname.startsWith('/dashboard');
+      const url = new URL(request.url); // Use URL to extract the pathname
+      const isOnDashboard = url.pathname.startsWith('/dashboard');
+
       if (isOnDashboard) {
         if (isLoggedIn) return true;
         return false; // Redirect unauthenticated users to login page
       } else if (isLoggedIn) {
-        return Response.redirect(new URL('/dashboard', nextUrl));
+        return NextResponse.redirect(new URL('/dashboard', request.url));
       }
       return true;
     },
   },
   providers: [], // Add providers with an empty array for now
-} satisfies NextAuthConfig;
+};
